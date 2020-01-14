65ca0993b244aeac43125883b4ba67e4f1a7e71d
Vinicius Crestani <vinicr@gmail.com>
2020-01-14 16:11:17 -0300

    Navegacao usando saga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -10,15 +10,17 @@ import Header from './components/Header';
 import store from './store';
 import GlobalStyle from './styles/global';
 
+import history from './services/history';
+
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
---
+++ src/services/history.js
@@ -0,0 +1,5 @@
+import { createBrowserHistory } from 'history';
+
+const histoty = createBrowserHistory();
+
+export default histoty;
